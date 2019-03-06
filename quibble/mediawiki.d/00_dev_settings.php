ca8d1bc5ba8c73ec36f53441ab3f7cf1d35350b2
Timo Tijhof <krinklemail@gmail.com>
2019-03-06 18:17:34 +0000

    mediawiki.d: Improve docs about dev settings and combine env sections
    Also remove some comments in the back-compat code path, because this is an
    exact copy of the core file. Code comments tend to help maintenance,
    but in this case I think was wrongly encouraging people to edit it,
    making people forget that it's not actually used in most cases,
    because it is only for older MW branches. New changes should be made
    to core instead.
    
    Also document since which version, so that we can easily remove it
    at some point.
    
    Change-Id: I1d0c099cd96323c91fff8579d117b9caa8c67fd4

--- quibble/mediawiki.d/00_dev_settings.php
+++ quibble/mediawiki.d/00_dev_settings.php
@@ -2,12 +2,31 @@
 # Quibble MediaWiki configuration
 # Originally copied from integration/jenkins.git:/mediawiki/conf.d/
 
+/**
+ * Process environment variables
+ */
+
+// Get configured log directory
 if ( getenv( 'MW_LOG_DIR' ) ) {
 	$quibbleLogDir = getenv( 'MW_LOG_DIR' );
 } elseif ( getenv( 'WORKSPACE' ) ) {
 	$quibbleLogDir = getenv( 'WORKSPACE' ) . '/log';
 } else {
 	$quibbleLogDir = __DIR__ . '/../log';
+}
+
+// Ensure MW_INSTALL_PATH is set
+if ( !getenv( 'MW_INSTALL_PATH' ) ) {
+	if ( is_dir( getenv( 'WORKSPACE' ) . '/src/mediawiki/core' ) ) {
+		// The new pattern as of 2014, as used by Zuul cloner in Jenkins,
+		// and in Quibble, is to clone MediaWiki at /src/mediawiki/core,
+		// aka "/src/$ZUUL_PROJECT".
+		putenv( 'MW_INSTALL_PATH='. getenv( 'WORKSPACE' ) . '/src/mediawiki/core' );
+	} else {
+		// Legacy
+		// CI jobs that don't use Zuul cloner, clone core directly at WORKSPACE
+		putenv( 'MW_INSTALL_PATH='. getenv( 'WORKSPACE' ) );
+	}
 }
 
 /**
@@ -17,20 +36,14 @@ if ( is_file( "$IP/includes/DevelopmentSettings.php" ) ) {
 	putenv( "MW_LOG_DIR=$quibbleLogDir" );
 	require_once "$IP/includes/DevelopmentSettings.php";
 } else {
-	// Compatibility with older MediaWiki branches
-
-	// Debugging: PHP
+	// Support: Mediawiki 1.30 and earlier
 	error_reporting( -1 );
 	ini_set( 'display_errors', 1 );
-
-	// Debugging: MediaWiki
 	$wgDevelopmentWarnings = true;
 	$wgShowDBErrorBacktrace = true;
 	$wgShowExceptionDetails = true;
 	$wgShowSQLErrors = true;
-	$wgDebugRawPage = true; // bug 47960
-
-	// Debugging: Logging
+	$wgDebugRawPage = true;
 	if ( $wgCommandLineMode ) {
 		$wgDebugLogFile = "$quibbleLogDir/mw-debug-cli.log";
 	} else {
@@ -41,12 +54,13 @@ if ( is_file( "$IP/includes/DevelopmentSettings.php" ) ) {
 	$wgDebugLogGroups['ratelimit'] = "$quibbleLogDir/mw-ratelimit.log";
 	$wgDebugLogGroups['exception'] = "$quibbleLogDir/mw-exception.log";
 	$wgDebugLogGroups['error'] = "$quibbleLogDir/mw-error.log";
-	// Back-compat
 	$wgRateLimitLog = $wgDebugLogGroups['ratelimit'];
 }
 
 /**
- * Experimental settings that we're trying for Jenkins
+ * Experimental settings
+ *
+ * Stricter configurations we're trying out by enforcing in CI first,
  * before they become defaults in MediaWiki core
  */
 
