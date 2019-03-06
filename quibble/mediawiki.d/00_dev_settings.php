c5856b88894d77e7d2d9642590ad157dd271b6c9
Timo Tijhof <krinklemail@gmail.com>
2019-03-06 18:12:09 +0000

    mediawiki.d: Avoid vars that look like core or wmf names
    Change-Id: I8fd54007c33ec92dfa9610d0e3bf70acf0cd9777

--- quibble/mediawiki.d/00_dev_settings.php
+++ quibble/mediawiki.d/00_dev_settings.php
@@ -1,20 +1,20 @@
 <?php
 # Quibble MediaWiki configuration
-# Snippet originally coming from integration/jenkins.git:/mediawiki/conf.d/
+# Originally copied from integration/jenkins.git:/mediawiki/conf.d/
 
 if ( getenv( 'MW_LOG_DIR' ) ) {
-	$wmgMwLogDir = getenv( 'MW_LOG_DIR' );
+	$quibbleLogDir = getenv( 'MW_LOG_DIR' );
 } elseif ( getenv( 'WORKSPACE' ) ) {
-	$wmgMwLogDir = getenv( 'WORKSPACE' ) . '/log';
+	$quibbleLogDir = getenv( 'WORKSPACE' ) . '/log';
 } else {
-	$wmgMwLogDir = __DIR__ . '/../log';
+	$quibbleLogDir = __DIR__ . '/../log';
 }
 
 /**
  * Development settings
  */
 if ( is_file( "$IP/includes/DevelopmentSettings.php" ) ) {
-	putenv( "MW_LOG_DIR=$wmgMwLogDir" );
+	putenv( "MW_LOG_DIR=$quibbleLogDir" );
 	require_once "$IP/includes/DevelopmentSettings.php";
 } else {
 	// Compatibility with older MediaWiki branches
@@ -32,15 +32,15 @@ if ( is_file( "$IP/includes/DevelopmentSettings.php" ) ) {
 
 	// Debugging: Logging
 	if ( $wgCommandLineMode ) {
-		$wgDebugLogFile = "$wmgMwLogDir/mw-debug-cli.log";
+		$wgDebugLogFile = "$quibbleLogDir/mw-debug-cli.log";
 	} else {
-		$wgDebugLogFile = "$wmgMwLogDir/mw-debug-www.log";
+		$wgDebugLogFile = "$quibbleLogDir/mw-debug-www.log";
 	}
 	$wgDebugTimestamps = true;
-	$wgDBerrorLog = "$wmgMwLogDir/mw-dberror.log";
-	$wgDebugLogGroups['ratelimit'] = "$wmgMwLogDir/mw-ratelimit.log";
-	$wgDebugLogGroups['exception'] = "$wmgMwLogDir/mw-exception.log";
-	$wgDebugLogGroups['error'] = "$wmgMwLogDir/mw-error.log";
+	$wgDBerrorLog = "$quibbleLogDir/mw-dberror.log";
+	$wgDebugLogGroups['ratelimit'] = "$quibbleLogDir/mw-ratelimit.log";
+	$wgDebugLogGroups['exception'] = "$quibbleLogDir/mw-exception.log";
+	$wgDebugLogGroups['error'] = "$quibbleLogDir/mw-error.log";
 	// Back-compat
 	$wgRateLimitLog = $wgDebugLogGroups['ratelimit'];
 }
