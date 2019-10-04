406fb1b2476e365f7353c51cfb3b2ec1f9bceff3
Antoine Musso <hashar@free.fr>
2019-10-04 14:58:42 +0200

    Migrate to setup.cfg
    Apparently the new way of describing a python module is to use setup.cfg
    instead of passing parameters to setuptools.setup().  I find it more
    convenient (raw text file instead of python).
    
    Tested by comparing the generated dist (python3 setup.py dist) versus
    HEAD^. That is a noop.  Also confirmed the 'quibble' command is
    installed.
    
    Change-Id: Ia6c1b4947653200b57da2acfbb39909f318329d7

--- setup.py
+++ setup.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='quibble',
-    packages=find_packages(),
-    install_requires=[
-        # For zuul-cloner
-        'extras',
-        'six',
-        'PyYAML',
-        # 2.1.2 has performances degradation
-        # Solved in 2.1.8 but jessie-backports and stretch have 2.1.1
-        #
-        # zuul dcafeb6038b96a0095976963814cc29f98520995
-        # https://github.com/gitpython-developers/GitPython/issues/605
-        #
-        # So we want either <2.1.2 or >2.1.7
-        'GitPython<2.2.0,!=2.1.2,!=2.1.3,!=2.1.4,!=2.1.5,!=2.1.6,!=2.1.7'
-        ],
-    package_data={
-        'quibble.mediawiki': ['*.php'],
-    },
-    entry_points={
-        'console_scripts': [
-            'quibble = quibble.cmd:main'
-        ],
-    }
-)
+# See setup.cfg
+setup()
