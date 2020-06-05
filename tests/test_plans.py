159edb213adf3f5147b313c35ab7d9dd90fca5b0
Antoine Musso <hashar@free.fr>
2020-06-05 23:38:53 +0200

    High level testing for the execution plan
    I have unfortunately reverted a couple changes by Adam Wight when trying
    to optimize the build plan for T211702.
    
    888218fb477 Clone only the target project at first
    7bfef6e5bda Wipe repo with non-git commands
    
    The plan did not work when reusing a workspace (--skip-zuul). The
    intended behavior can be expressed as a test in tests/test_cmd.py,
    specially when we just want to assert a specific command is there with
    proper arguments.
    
    Eventually I though it would be good to have some high level acceptance
    tests for common usages. They could later on be included in the
    documentation.
    
    This change introduces short yaml files to feed environment variables
    and command line arguments to Quibble. The test generate the build plan
    and assert it is strictly equal to the provided plan.
    
    Bug: T211702
    Change-Id: I17bac505580f6a404920cf6e6ac228cbc6a4d1ec

---
+++ tests/test_plans.py
@@ -0,0 +1,34 @@
+from glob import glob
+from unittest import mock
+import os.path
+
+import pytest
+from quibble import cmd
+import yaml
+
+PLANS_DIR = os.path.join(os.path.dirname(__file__), 'plans')
+
+
+def plans():
+    for plan_filename in sorted(glob(os.path.join(PLANS_DIR, '*.yaml'))):
+        with open(plan_filename) as f:
+            test_data = yaml.safe_load(f)
+
+        yield pytest.param(
+            test_data.get('plan'),
+            test_data.get('args', []),
+            test_data.get('env', {}),
+            id=os.path.relpath(plan_filename, PLANS_DIR)
+            )
+
+
+@pytest.mark.parametrize('expected,args,env', plans())
+def test_plan(expected, args, env):
+
+    with mock.patch.dict('os.environ', env, clear=True):
+        cmd_args = cmd.parse_arguments(args)
+        plan = cmd.QuibbleCmd().build_execution_plan(cmd_args)
+
+    printable_plan = [str(command) for command in plan]
+
+    assert printable_plan == expected
